cec248bfd302e163596df149767a386056caa945
dinh.truong <dinh.truong@zien.vn>
2022-08-02 16:14:49 +0700

    change icon

--- app/src/main/java/com/ocsen/onestep/ui/adapter/LocalImageViewHolder.kt
+++ app/src/main/java/com/ocsen/onestep/ui/adapter/LocalImageViewHolder.kt
@@ -1,11 +1,13 @@
 package com.ocsen.onestep.ui.adapter
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.data.local.entities.LocalImage
+import com.ocsen.onestep.Utils.DateTimeUtils
 import com.ocsen.onestep.databinding.LocalImageBinding
 
 open class LocalImageViewHolder(
@@ -32,6 +34,8 @@ open class LocalImageViewHolder(
             onItemClickListener.onItemClick(item)
         })
         binding.tvLatLng.text = "${item?.lat} -${item?.lon}"
+        binding.timeStamp.text= DateTimeUtils.getDateString(item.timestamp)
+        Log.d("IMAGE_TIMESTAMP",DateTimeUtils.getDateString(item.timestamp))
 //        binding.txtTitleEvent.text = Util.parseHtmlToStyledTextOrEmpty(item.title)
 //        binding.descEvent.text = Util.parseHtmlToStyledTextOrEmpty(item.shortDesc)
 //        binding.btnContinue.setOnClickListener { onItemClickListener.onItemClick(item) }
